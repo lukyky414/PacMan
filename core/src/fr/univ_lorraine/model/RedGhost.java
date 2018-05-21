188a27867bf46f9fde21550663dd296aa48eb772
Charlotte LANUEL <charlotte.lanuel@hotmail.fr>
2018-05-21 15:53:18 +0200

    RedGhost init

---
+++ core/src/fr/univ_lorraine/model/RedGhost.java
@@ -0,0 +1,16 @@
+package fr.univ_lorraine.model;
+
+import fr.univ_lorraine.model.Movable;
+import fr.univ_lorraine.model.World;
+import com.badlogic.gdx.math.Vector2;
+
+public class RedGhost extends Movable {
+
+    private int nextDir;
+
+    public RedGhost(Vector2 pos, World w){
+        super(pos, w);
+        nextDir = 
+    }
+
+}
